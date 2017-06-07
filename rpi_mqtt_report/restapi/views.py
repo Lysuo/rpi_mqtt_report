39ee2516be016295b84ce0773e8d71e5331577cd
Jerome Gueret <jer.gueret@gmail.com>
2017-06-07 19:07:18 +0100

    restapi - GET function for sensor entries - done

--- rpi_mqtt_report/restapi/views.py
+++ rpi_mqtt_report/restapi/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from datetime import datetime as dt
+import pytz
 
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
@@ -15,6 +17,28 @@ from threading import Thread
 class SensorDHEntryRest(APIView):
 
   def get(self, request, format=None):
-    l = SensorDHEntry.objects.all()
+    dy = request.META.get('HTTP_REQDATEY')
+    dm = request.META.get('HTTP_REQDATEM')
+    dd = request.META.get('HTTP_REQDATED')
+    tz_str = request.META.get('HTTP_TZINFO')
+
+    tz_r = pytz.timezone(tz_str)
+    tz_utc = pytz.timezone('UTC')
+
+    start_date = tz_r.localize(dt(year=int(dy), month=int(dm), day=int(dd)))
+    end_date = start_date.replace(hour=23, minute=59)
+
+    start_date = start_date.astimezone(tz_utc)
+    end_date = end_date.astimezone(tz_utc)
+
+    print start_date
+    print end_date
+
+    #l = SensorDHEntry.objects.all()
+    l = SensorDHEntry.objects.filter(mDate__range=(start_date, end_date))
+
+    for e in l:
+      e.mDate = (e.mDate).astimezone(tz_r)
+
     serializer = SensorDHEntrySerializer(l, many=True)
     return Response(serializer.data)
