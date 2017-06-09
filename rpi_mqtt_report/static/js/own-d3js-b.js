78342a9f197d1bb87648878a48439977ad65a0c8
Jerome Gueret <jer.gueret@gmail.com>
2017-06-09 17:00:18 +0100

    humidity graph

--- rpi_mqtt_report/static/js/own-d3js-b.js
+++ rpi_mqtt_report/static/js/own-d3js-b.js
@@ -4,84 +4,63 @@ var svg = 0;
 function drawBarChart() {
 
   if (svg != 0) {
-svg.selectAll('*').remove();
+    svg.selectAll('*').remove();
   }
 
-// set the dimensions of the canvas
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+  // set the dimensions of the canvas
+  var margin = {top: 20, right: 20, bottom: 70, left: 40},
+      width = 600 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
 
+  // set the ranges
+  var x = d3.scale.linear().range([0, width]).domain([0,24]);
+  var yt = d3.scale.linear().range([height, 0]).domain([15,24]);
+  var yh = d3.scale.linear().range([height, 0]).domain([50,60]);
 
-// set the ranges
-var x = d3.scale.linear().range([0, width]).domain([0,24]);
-var y = d3.scale.linear().range([height, 0]).domain([15,24]);
-
-// define the axis
-  var xAxis = d3.svg.axis()
-.scale(x)
-  .orient("bottom")
-
-
-  var yAxis = d3.svg.axis()
-.scale(y)
-  .orient("left");
-//  .ticks(10);
-
+  // define the axis
+  var xAxis = d3.svg.axis().scale(x).orient("bottom");
+  var ytAxis = d3.svg.axis().scale(yt).orient("left");
+  var yhAxis = d3.svg.axis().scale(yh).orient("right");
 
   // add the SVG element
   svg = d3.select("#visualisation").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", 
-      "translate(" + margin.left + "," + margin.top + ")");
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g") .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-// load the data
-/*d3.json("data.json", function(error, data) {
-
-  data.forEach(function(d) {
-    d.Letter = d.hour;
-    d.Freq = +d.temp;
-  });*/
-
-  // scale the range of the data
-//  x.domain(data.map(function(d) { return d.hour; }));
-//  y.domain([0, d3.max(data, function(d) { return d.temp; })]);
-
-  // add axis
+  // Add axis
   svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxis)
-  .selectAll("text")
-  .style("text-anchor", "end")
-  .attr("dx", "-.8em")
-  .attr("dy", "-.55em")
-  .attr("transform", "rotate(-90)" );
-
-svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis)
-  .append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 5)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end")
-  .text("Temperature");
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(ytAxis); 
+  svg.append("g")       
+    .attr("class", "y axis")  
+    .attr("transform", "translate(" + width + " ,0)") 
+    .call(yhAxis);
 
 
-// Add bar chart
-svg.selectAll("bar")
-  .data(dataBarChart)
-  .enter().append("rect")
-  .attr("class", "bar")
-  .attr("x", function(d) { return x(d.hour); })
-  .attr("width", 3)
-  .attr("y", function(d) { return y(d.temp); })
-  .attr("height", function(d) { return height - y(d.temp); });
+  // Add temperature bar chart
+  svg.selectAll("bar")
+    .data(dataBarChart)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.hour); })
+    .attr("width", 3)
+    .attr("y", function(d) { return yt(d.temp); })
+    .attr("height", function(d) { return height - yt(d.temp); });
 
-//});
+  // Add humidity line
+  var humLine = d3.svg.line()
+    .x(function(d) { return x(d.hour); })
+    .y(function(d) { return yh(d.hum); })
+    .interpolate("basis");
 
+  svg.append("path")
+    .style("stroke", "red")
+    .attr("d", humLine(dataBarChart))
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');
 };
